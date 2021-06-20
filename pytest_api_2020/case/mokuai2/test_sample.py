d1570fa28c40e7290dc568706187d616c41e91b3
boxing20 <704466925@qq.com>
2021-06-21 02:01:42 +0800

    second commit

--- pytest_api_2020/case/mokuai2/test_sample.py
+++ pytest_api_2020/case/mokuai2/test_sample.py
@@ -1,10 +1,15 @@
 import pytest
+import pytest_assume
 
 def add(x):
     return x + 1
 
 def test_add():
-    assert add(1) == 2
+    pytest.assume(add(2) == 2)
+    pytest.assume(add(2) == 3)
+
+def test_add2():
+    pytest.assume(add(1) == 2)
 
 if __name__ == '__main__':
-    pytest.main()
+    pytest.main(["-v","./test_sample.py"])
